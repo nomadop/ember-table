9ebc079ee8c93e5d23e2d3fc3e25eff47209953a
cuiyansong <yscui@thoughtworks.com>
2015-07-28 15:55:33 +0800

    Update assert descriptions

--- tests/unit/models/column-definition-test.js
+++ tests/unit/models/column-definition-test.js
@@ -86,30 +86,30 @@ module('sort with data type', {
 
 test('sort column when content value type is decimal', function (assert) {
   column.dataType = "decimal";
-  assert.equal(column.sortFn({col: "15.27"}, {col: "15.28"}), -1, "15.27 should be smaller than 15.28");
+  assert.equal(column.sortFn({col: "15.27"}, {col: "15.28"}), -1, "‘15.27’ should be smaller than ‘15.28’");
 });
 
 test('sort column when content value type is decimal', function (assert) {
   column.dataType = "decimal";
-  assert.equal(column.sortFn({col: "3"}, {col: "12.1"}), -1, "3 should be smaller than 12.1");
+  assert.equal(column.sortFn({col: "3"}, {col: "12"}), -1, "‘3’ should be smaller than ‘12’");
 });
 
 test('sort column when content value type is string', function (assert) {
   column.dataType = "string";
-  assert.equal(column.sortFn({col: "ember"}, {col: "table"}), -1, "string of ember should be smaller than string of table");
+  assert.equal(column.sortFn({col: "ember"}, {col: "table"}), -1, "‘ember’ should be smaller than ‘table’");
 });
 
 test('sort column when content value type is datetime', function (assert) {
   column.dataType = "datetime";
-  assert.equal(column.sortFn({col: "20150727 12:00"}, {col: "20150728 9:00"}), -1, "datetime of 20150727 12:00 is smaller than datetime of 20150728 9:00");
+  assert.equal(column.sortFn({col: "20150727 12:00"}, {col: "20150728 9:00"}), -1, "‘20150727 12:00’ is smaller than ‘20150728 9:00’");
 });
 
 test('sort column when content value type is percentage', function (assert) {
   column.dataType = "percentage";
-  assert.equal(column.sortFn({col: "2%"}, {col: "3%"}), -1, "2% should be smaller than 3%");
+  assert.equal(column.sortFn({col: "2%"}, {col: "3%"}), -1, "‘2%’ should be smaller than ‘3%’");
 });
 
 test('sort column when content value type is percentage', function (assert) {
   column.dataType = "percentage";
-  assert.equal(column.sortFn({col: "3.1%"}, {col: "21.4%"}), -1, "3.1% should be smaller than 21.4%");
+  assert.equal(column.sortFn({col: "3%"}, {col: "12%"}), -1, "‘3%’ should be smaller than ‘12%’");
 });
